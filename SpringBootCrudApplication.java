49209881e2ac939000572e9f127ce85aa9a1e247
AyushK95 <56297276+AyushK95@users.noreply.github.com>
2020-11-17 17:59:00 +0530

    Add files via upload

---
+++ SpringBootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.Spring_CRUD_Application.SpringBoot_Crud_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudApplication.class, args);
+	}
+
+}
